4b05ea9c482dd50a79b98385402e5148173a76bc
Andrew Tran <tandrew468@gmail.com>
2022-08-13 21:27:36 +1000

    Updates

---
+++ precache-manifest.93a80367535205b9a1708e1ae03b8670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "994524da0bb3b15b3753",
+    "url": "/tandrew468-gh-page/static/css/main.b4a68431.chunk.css"
+  },
+  {
+    "revision": "994524da0bb3b15b3753",
+    "url": "/tandrew468-gh-page/static/js/main.994524da.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/tandrew468-gh-page/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/tandrew468-gh-page/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "49d90680e8f09495dc39",
+    "url": "/tandrew468-gh-page/static/js/runtime~main.49d90680.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tandrew468-gh-page/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5a278852509c257de93feb265170a7ad",
+    "url": "/tandrew468-gh-page/index.html"
+  }
+];
